238a3a10ed51b86d073c0e248d8f5af2584beb1b
Lucas Nunes <89349452+Lucasaltdel@users.noreply.github.com>
2024-11-27 20:08:54 -0300

    Add files via upload

---
+++ ProjetoPlayer/index.php
@@ -0,0 +1,36 @@
+<?php
+require_once ('classes/Player.php');
+require_once ('classes/Ataque.php');
+require_once ('classes/Defesa.php');
+require_once ('classes/Magia.php');
+
+
+$player = new Player("Forreλner", 1);
+
+
+$itemAtaque1 = new Ataque("Espada", 7, "Ataque");
+$itemDefesa1 = new Defesa("Escudo", 4, "Defesa");
+$itemMagia1 = new Magia("Vara", 2, "Magia");
+
+
+echo "Coletando itens para {$player->getNickname()}:<br>";
+$player->coletarItem($itemAtaque1);
+$player->coletarItem($itemDefesa1);
+$player->coletarItem($itemMagia1);
+echo "<br>";
+
+
+echo "Inventário de {$player->getNickname()}:<br>";
+$player->getInventario()->listar();
+echo "<br>";
+
+
+echo "{$player->getNickname()} está subindo de nível!<br>";
+$player->subirNivel();
+echo "Novo nível: " . $player->getNivel() . "<br>";
+echo "Nova capacidade do inventário: " . $player->getInventario()->getCapacidadeMaxima() . "<br>";
+echo "<br>";
+?>
+
+
+
